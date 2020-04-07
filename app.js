7130e8844d6093bc0fd543f90404bb12845fbeb1
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-04-07 15:08:07 -0700

    changed save img and src functions

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-var photoInput = document.getElementById('file-id').value
+// var photoInput = document.getElementById('file-id').value
+// var photoInput = document.getElementById("file-id").files[0];
+
 var createMeme = document.getElementById('submit-button')
 
+// local storage array
+var memeArr = JSON.parse(localStorage.getItem("memeArr")) || [];
+// var memeArr = [];
+
+
+
 var memeObj = {
-  photo: undefined,
+  photoInput: undefined,
+  photoSrc: undefined,
+
+  imgData: undefined,
   imgCanvas: undefined,
   imgContext: undefined,
-//   imgCanvasWidth: undefined,
-//   imgCanvasHeight: undefined,
-
-  dateURL: undefined,
+  imgCanvasWidth: undefined,
+  imgCanvasHeight: undefined,
+// dateURL: undefined,
   topText: undefined,
   bottomText: undefined,
 };
+
+var canvasImg = {
+    canvas: undefined,
+    src: undefined,
+    width: 200,
+    height: 200,
+}
 
 // add event listener to addPhoto
 
@@ -23,30 +40,76 @@ var memeObj = {
 // })
 
 // set photo value
-function photoSrc (e) {
-    var photoInput = document.getElementById("file-id").files[0].name;
+
+
+function photoSrc () {
+    memeObj.photoInput = document.getElementById("file-id").files[0];
     // var photoInput = document.getElementById("file-id");
+    memeObj.photoSrc = document.getElementById("file-id").files[0].name;
+    image = getBase64Image(memeObj.photoInput)
+    // console.log("photoSrc:" + memeObj.photoSrc);
+    // const ctx = photoInput.getContext("2d");
+    // console.log(ctx)
+    // console.log(photoInput);
+    // console.log(height)
 
-    memeObj.photo = photoInput;
+    // memeObj.photo = photoInput;
 }
 
-function memeCanvas() {
-    memeObj.imgCanvas = document.createElement("canvas")
-    memeObj.imgContext = memeObj.imgCanvas.getContext("2d");
+function saveImg(photo) {
+    memeObj.imgData = getBase64Image(photo)
     console.log(memeObj)
-    console.log(memeObj.photo.width);
-
-    memeObj.imgCanvas.width = memeObj.photo.width;
-    memeObj.imgCanvas.height = memeObj.photo.height;
-    memeObj.imgContext.drawImage(
-      memeObj.photo,
-      0,
-      0,
-      memeObj.photo.width,
-      memeObj.photo.height
-    );
-    // console.log(memeObj)
+    memeArr.push(memeObj);
+    // console.log("memeArr")
+    // console.log(memeArr[0]);
+    localStorage.setItem("memeArr", JSON.stringify(memeArr));
 }
+
+
+
+function getBase64Image(img) {
+    console.log("at get base")
+    var imgCanvas = document.createElement("canvas");
+    imgCanvas.width = img.width
+    imgCanvas.height = img.height
+
+    var imgContext = imgCanvas.getContext("2d")
+    imgContext.drawImage(img, 0, 0)
+
+    var dataURL = imgCanvas.toDataURL("image/jpeg")
+    
+
+    
+
+
+
+    // canvas.width = memeObj.photo.width;
+    // canvas.height = memeObj.photo.height;
+    //var ctx = canvas.getContext("2d");
+   // ctx.drawImage(memeObj.photo, canvasImg.width, canvasImg.width);
+
+    // var dataURL = canvas.toDataURL("image/png");
+    // console.log(dataURL);
+    return dataURL.replace(/^data:image\/(png|jpg);base64,/,"")
+};
+
+// function memeCanvas() {
+//     memeObj.imgCanvas = document.createElement("canvas")
+//     memeObj.imgContext = memeObj.imgCanvas.getContext("2d");
+//     console.log(memeObj)
+//     console.log(memeObj.photo.width);
+
+//     memeObj.imgCanvas.width = memeObj.photo.width;
+//     memeObj.imgCanvas.height = memeObj.photo.height;
+//     memeObj.imgContext.drawImage(
+//       memeObj.photo,
+//       0,
+//       0,
+//       memeObj.photo.width,
+//       memeObj.photo.height
+//     );
+//     // console.log(memeObj)
+// }
 
 
 function topTextInput() {
@@ -65,16 +128,40 @@ createMeme.addEventListener('click', function(e) {
     photoSrc()
     topTextInput();
     bottomTextInput();
-    // console.log(memeObj)
-    memeCanvas()
+    saveImg()
+    appendMemeToDiv();
 })
 
-//save to local storage using imgCanvas
+//save to local storage 
+
+function appendMemeToDiv () {
+    var memeGallery = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+    // console.log(memeGallery);
+    
+
+    const image = new Image(60, 45);
+    image.src = './pictures/example.jpg'
+    document.body.appendChild(image)
+    // image.onload = drawImageActualSize;
+    src = JSON.parse(localStorage.getItem("memeArr"))
+    console.log(src)
+    // image.src = 
+    
+    // memeGallery.append(canvasImg);
 
 
-// function createMemePhoto () {
+// function drawImageActualSize () {
+//     canvas.width = this.naturalWidth;
+//     canvas.height = this.naturalHeight;
+//     ctx.drawImage(this, 0, 0, this.width, this.height);
+// }
+
+}
+
+/// function createMemePhoto () {
 //     var newMeme = document.createElement("img")
-//     newMeme.src = memeObj.photo
+//     newMeme.src = memeObj.photoSrcmeObj.photo;
 //     console.log(newMeme)
 // }
 
